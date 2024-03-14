d776479260f9875b2e52dcc353857aea980e1539
SaurabhT91 <saurabhtidgam1991@gmail.com>
2024-03-14 18:58:38 +0530

    creating first page and login page
    without css and effects

--- MeetSpaceClient/src/App.jsx
+++ MeetSpaceClient/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet, Link } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,27 +10,13 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 className="appName">Welcome to MeetSpace</h1>
+        <h2>Book Meeting spaces</h2>
+        <Link to={`login`}>login</Link>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Outlet />
     </>
-  )
+  );
 }
 
 export default App
