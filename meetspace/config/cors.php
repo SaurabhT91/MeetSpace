18c1e7b0db4597f627bbf1e403c881e81a895376
SaurabhT91 <saurabhtidgam1991@gmail.com>
2025-05-15 22:48:53 +0530

    Update cors.php

--- meetspace/config/cors.php
+++ meetspace/config/cors.php
@@ -2,7 +2,7 @@
 
 return [
 
-    /*
+/*
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
@@ -15,11 +15,13 @@ return [
     |
     */
 
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://www.meetspace.saurabhtidgam.info',],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +31,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
