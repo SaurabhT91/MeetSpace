191a6738f1ae719857a0d3e16b838feb002744dc
SaurabhT91 <saurabhtidgam1991@gmail.com>
2025-05-16 00:04:00 +0530

    Update authAPI.js

--- MeetSpaceClient/src/services/authAPI.js
+++ MeetSpaceClient/src/services/authAPI.js
@@ -1,9 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setUser, setAccessToken, setError } from "../slices/authSlice";
+const API_BASE = import.meta.env.VITE_API_URL;
 
 export const authApi = createApi({
   reducerPath: "authApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/api/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: API_BASE }),
   endpoints: (builder) => ({
     loginUser: builder.mutation({
       query: (credentials) => ({
